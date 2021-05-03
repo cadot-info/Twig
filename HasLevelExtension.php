0a3a473923262f48395f99f5df7c18989f7258d7
michael <michael@cadot.info>
2021-05-03 20:42:50 +0200

    uploadmax

---
+++ HasLevelExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+use Doctrine\ORM\EntityManagerInterface;
+
+class HasLevelExtension extends AbstractExtension
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    public function getFunctions(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFunction('haslevel', [$this, 'haslevel']),
+        ];
+    }
+
+
+    public function haslevel($id)
+    {
+        $user = $this->em->getRepository("App:User");
+        return $user->find($id)->getRoles()[0];
+    }
+}
---
+++ UploadMaxExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class UploadMaxExtension extends AbstractExtension
+{
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('upload_max', [$this, 'max']),
+        ];
+    }
+
+    public function max()
+    {
+        $max_upload = (int)(ini_get('upload_max_filesize'));
+        $max_post = (int)(ini_get('post_max_size'));
+        $memory_limit = (int)(ini_get('memory_limit'));
+        return (min($max_upload, $max_post, $memory_limit));
+    }
+}
