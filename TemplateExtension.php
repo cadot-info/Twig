9e72db850966df261ca6b62f78ed578a3f250e31
michael <michael@cadot.info>
2021-05-05 00:17:54 +0200

    maj

---
+++ TemplateExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class TemplateExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('formrow', [$this, 'formrow'], ['is_safe' => ['html']]),
+            new TwigFunction('formcol', [$this, 'formcol'], ['is_safe' => ['html']]),
+            new TwigFunction('formend', [$this, 'formend'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function formrow()
+    {
+        return ('<div class="row">');
+    }
+    public function formend()
+    {
+        return ('</div>');
+    }
+
+    public function formcol($value)
+    {
+        return '<div class="col-' . $value . '">';
+    }
+}
