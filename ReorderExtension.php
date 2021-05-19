227cc2453d5f28a59340b8a2d1dd771ab3892d8e
michael <michael@cadot.info>
2021-05-19 16:07:04 +0200

    sortable

---
+++ ReorderExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ReorderExtension extends AbstractExtension
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('reorder', [$this, 'reorder']),
+        ];
+    }
+
+    public function reorder($repository)
+    {
+        $array = [];
+        $objet = $this->em->getRepository("App:" . ucfirst($repository))->findall();
+        $sortable = explode(',', $this->em->getRepository("App:Sortable")->findOneBy(['entite' => $repository])->getordre()); //tableau des ordres
+        foreach ($sortable as $index => $num) {
+            $res =  array_filter(
+                $objet,
+                function ($e) use (&$num) {
+                    return $e->getId() == $num;
+                }
+            );
+            $array[$index] = reset($res);
+        }
+        return $array;
+    }
+}
