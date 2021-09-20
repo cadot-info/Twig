65dccc3fd45744be689fca968c6e6c432f63d541
michael <michael@cadot.info>
2021-09-20 17:13:50 +0100

    ok

--- EditorjsExtension.php
+++ EditorjsExtension.php
@@ -43,36 +43,36 @@ class EditorjsExtension extends AbstractExtension
 
         $tabs = json_decode($json);
         //on liste les objets
-        foreach ($tabs->blocks as $num => $tab) {
-            $data = '';
-            switch ($tab->type) {
-                    // case 'paragraph':
-                    // case 'header':
-                    //     $data = $tab->data->text;
-                    //     if (substr(html_entity_decode($data), 0, 2) == '¤') $tabs->blocks[$num]->data->text = substr($tab->data->text, 2);
-                    //     break;
-                    // case 'image':
-                    //     $data = $tab->data->caption;
-                    //     if (substr(html_entity_decode($data), 0, 2) == '¤') $tabs->blocks[$num]->data->caption = substr($tab->data->caption, 2);
-                    //     $width = getimagesize(getcwd() . $tab->data->url)[0];
-                    //     //limit width
-                    //     if ($width > 1920) {
-                    //         $imagineCacheManager = $this->container->get('liip_imagine.cache.manager');
-                    //         $resolvedPath = $imagineCacheManager->getBrowserPath($tab->data->url, 'fullhd');
-                    //         $tabs->blocks[$num]->data->url = $resolvedPath;
-                    //     }
-                    //     break;
-            }
-            //si pas le droit de voir on supprime
-            if (strpos($data, '¤') !== false)
-                if (substr(html_entity_decode($data), 0, 2) == '¤' and $this->roles == null)
-                    unset($tabs->blocks[$num]);
-        }
+        // foreach ($tabs->blocks as $num => $tab) {
+        //     $data = '';
+        //     switch ($tab->type) {
+        //         case 'paragraph':
+        //         case 'header':
+        //             $data = $tab->data->text;
+        //             if (substr(html_entity_decode($data), 0, 2) == '¤') $tabs->blocks[$num]->data->text = substr($tab->data->text, 2);
+        //             break;
+        //         case 'image':
+        //             $data = $tab->data->caption;
+        //             if (substr(html_entity_decode($data), 0, 2) == '¤') $tabs->blocks[$num]->data->caption = substr($tab->data->caption, 2);
+        //             $width = getimagesize(getcwd() . $tab->data->url)[0];
+        //             //limit width
+        //             if ($width > 1920) {
+        //                 $imagineCacheManager = $this->container->get('liip_imagine.cache.manager');
+        //                 $resolvedPath = $imagineCacheManager->getBrowserPath($tab->data->url, 'fullhd');
+        //                 $tabs->blocks[$num]->data->url = $resolvedPath;
+        //             }
+        //             break;
+        //     }
+        //     //si pas le droit de voir on supprime
+        //     if (strpos($data, '¤') !== false)
+        //         if (substr(html_entity_decode($data), 0, 2) == '¤' and $this->roles == null)
+        //             unset($tabs->blocks[$num]);
+        // }
 
-        $json = json_encode($tabs);
-        $html = null;
-        if ($tabs->blocks)
-            $html = new \Twig\Markup(Parser::parse($json)->toHtml(), 'UTF-8');
+        // $json = json_encode($tabs);
+        // $html = null;
+        // if ($tabs->blocks)
+        //     $html = new \Twig\Markup(Parser::parse($json)->toHtml(), 'UTF-8');
         //ajout des finctionnalitées propre à mickcrud
         //travaille sur les images en ajoutant un filtre liip
         return $html;
